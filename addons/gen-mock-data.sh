8115990e51539355552d0d3fc36569918370b040
c0m4r <github@wolfet.pl>
2026-03-01 16:45:14 +0100

    refine gen-mock-data.sh

--- addons/gen-mock-data.sh
+++ addons/gen-mock-data.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -e
+
+cd "$(dirname "$0")/.."
+
+echo "Building gen-mock-data..."
+
 CGO_ENABLED=0 go build \
     -trimpath \
     -ldflags="-s -w" \
     -buildvcs=false \
     -o gen-mock-data \
     ./cmd/gen-mock-data/
+
+echo "Done!"
